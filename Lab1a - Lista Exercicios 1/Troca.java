fee82889f4309574dfea90c17b39a1da0b5b4e43
lendy-pacheco <lendypacheco21@gmail.com>
2025-03-17 00:46:27 +0000

    Exercicio - Troca realizado

---
+++ Lab1a - Lista Exercicios 1/Troca.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Troca{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int n1 = scanner.nextInt();
+        System.out.print("Digite outro número: ");
+        int n2 = scanner.nextInt();
+
+        int aux;
+        aux = n1;
+        n1 = n2;
+        n2 = aux;
+        System.out.println("Troca realizada: "+ n1 + " " + n2);
+        scanner.close();      
+    }
+}
