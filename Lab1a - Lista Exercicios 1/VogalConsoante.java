0b0f943ad53ba256b4466442f88794a2f268e1a3
lendy-pacheco <lendypacheco21@gmail.com>
2025-03-20 18:48:31 +0000

    Exercicio1 - Vogal ou Consante realizado

---
+++ Lab1a - Lista Exercicios 1/VogalConsoante.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class VogalConsoante{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma letra: ");
+        String letra = scanner.nextLine();
+
+        String[] vogais = {"a", "e", "i", "o", "u","A","E","I","O","U"};
+        String[] consoantes = {"b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"};
+        int i;
+        for(i = 0; i < vogais.length; i++){
+            if(vogais[i].equals(letra)){
+                System.out.println("A letra " + letra + " é uma vogal");
+            }
+        }
+        for(i = 0; i < consoantes.length; i++){
+            if(consoantes[i].equals(letra)){
+                System.out.println("A letra " + letra + " é uma consoante");
+            }
+        }
+        scanner.close();
+    }
+}
