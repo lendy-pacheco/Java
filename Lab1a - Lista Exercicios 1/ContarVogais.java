52de5a09d77a06106706ab74c13bef1476193451
lendy-pacheco <lendypacheco21@gmail.com>
2025-03-20 23:19:42 +0000

    Exercicio 1 - Contar Vogais realizado

---
+++ Lab1a - Lista Exercicios 1/ContarVogais.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ContarVogais {
+    public static int contarVogais(String texto) {
+        int contador = 0;
+        texto = texto.toLowerCase(); 
+        for (char c : texto.toCharArray()) { 
+            if ("aeiou".indexOf(c) != -1) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma string: ");
+        String texto = scanner.nextLine();
+        System.out.println("Quantidade de vogais: " + contarVogais(texto));
+
+        scanner.close();
+    }
+}
