e3e9cfe5e667c20e4c9b97a6388e68a39b15cf09
lendy-pacheco <lendypacheco21@gmail.com>
2025-03-20 22:29:53 +0000

    Exercicio 1 - Multiplicação(somas) realizado

---
+++ Lab1a - Lista Exercicios 1/Multiplicacao.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Multiplicacao{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        float num1 = scanner.nextFloat();
+        System.out.print("Digite outro número: ");
+        int num2 = scanner.nextInt();
+        float resultado = mulComAdicao(num1, num2);
+        System.out.println("Resultado: "+ resultado);
+
+    }
+
+    static float mulComAdicao(float num1, int num2){
+        if(num2 == 0){
+            return 0;
+        }
+        else{
+            return num1 + mulComAdicao(num1, num2 -1);
+        }
+    }
+}
